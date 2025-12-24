2c5aa894119ce8992c0a129994890ae5fd11302f
Dariusz Filipiak <darekf77@gmail.com>
2025-12-24 13:57:33 +0100

    feat: singlet file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButton } from '@angular/material/button';
+import { mergeApplicationConfig } from '@angular/core';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Routes } from '@angular/router';
+
+
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
+];
+export const routes: Routes = [];
+
 
 @Component({
   selector: 'app-root',
@@ -11,3 +29,25 @@ import { MatButton } from '@angular/material/button';
 export class App {
   protected readonly title = signal('app21');
 }
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ]
+};
+
+
+const serverConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(withRoutes(serverRoutes))
+  ]
+};
+
+export const config = mergeApplicationConfig(appConfig, serverConfig);
